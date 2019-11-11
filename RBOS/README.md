7f595c1b334461c74ab83ba74c2e65cf6675640d
jyizheng <jyizheng@gmail.com>
2019-11-11 01:27:15 +0000

    add RBOS

---
+++ RBOS/README.md
@@ -0,0 +1,190 @@
+# RBOS
+ 为linux内核添加简易的基于角色的访问控制功能,系统基于LSM(Linux Security Module)模块;
+ 为了能够支持insmod和rmmod ,我们对内核做了一定的修改,将security.c里面的security_hook_heads结构 EXPORT_SYMBOLS了。
+# 系统要求
+本驱动在ubuntu 14.04 amd64 可以很好的运行;
+# 功能
+1. 定义好4类角色以及对应的权限
+配置文件存放:/etc/rbos/role_config
+
+文件格式如下：
+```
+角色名：权限;
+例如：
+recycler:SYSCALL_TASK_CREATE,SYSCALL_RMDIR,SYSCALL_MKDIR;
+```
+四个内置角色说明:
+
+|角色名|权限|
+|---|---|
+|recycler|SYSCALL_TASK_CREATE,SYSCALL_RMDIR,SYSCALL_MKDIR|
+|operator|SYSCALL_TASK_CREATE|
+|netmanger|SYSCALL_TASK_CREATE,SYSCALL_SOCKET,SYSCALL_CONNECT|
+|admin|SYSCALL_TASK_CREATE,SYSCALL_RMDIR,SYSCALL_MKDIR,SYSCALL_SOCKET,SYSCALL_CONNECT|
+
+2. 内置5种权限:
+
+  |权限名|含义|
+  |---|---|
+  |SYSCALL_SOCKET|调用socket,创建套接字的权限|
+  |SYSCALL_CONNECT|调用connect系统调用的权限|
+  |SYSCALL_MKDIR|创建目录的权限|
+  |SYSCALL_RMDIR|删除目录的权限|
+  |SYSCALL_TASK_CREATE|创建子进程|
+3. 用户角色分配,可以给用户分配一定的角色;
+配置文件存放:/etc/rbos/user_config
+
+文件格式如下
+```
+用户id:角色名;
+例如:
+1000:netmanager;
+1001:recycler;
+```
+因此，需要先使用cat /etc/passwd 查看当前系统不同用户的用户ID.
+# 编译方法
+1.  下载内核源码
+```
+cd /usr/src/
+wget https://launchpad.net/ubuntu/+archive/primary/+sourcefiles/linux-lts-xenial/4.4.0-31.50~14.04.1/linux-lts-xenial_4.4.0.orig.tar.gz
+tar -xvf linux-lts-xenial_4.4.0.orig.tar.gz
+```
+2.  对linux-4.4.0里面源码做修改
+/security/security.c 文件里面
+在最后添加:EXPORT_SYMBOL(security_hook_heads);
+
+3. 重新编译内核
+得到当前的内核版本：
+```
+uname -r
+```
+根据这个值可以得到当前使用的boot 配置文件
+```
+make mrproper	#取消代码树
+cp /boot/config-4.4.0-31-genric /usr/src/linux-4.4/.config #复制当前的系统的config到源码文件夹
+make menuconfig
+make clean
+make bzImage -j8
+make modules -j8
+make modules_install
+make install
+reboot
+```
+编译的时候会很长时间的,一般到10-20分钟。
+中途可能会报一些错误，主要是在makemenuconfig时需要安装libcurses,在make的时候需要安装libssl-dev.
+
+注意,在make install后,编译好的新内核会添加/lib/目录下,boot目录下也会有相应的镜像,但是此时需要修改grup 配置文件,然后update-grub2将系统的引导选择加载打开,这样我们才能选择以我们刚刚编译好的新内核启动。
+
+一般新内核在系统启动引导选项的Adanced选项里面！
+！！！！注意 一定要从新编译的内核启动，否则以下RBOS会编译不过。
+
+4. 编译RBOS代码
+获取源码：
+```
+git clone https://github.com/jmhIcoding/RBOS.git
+cd RBOS
+git checkout lsm_samle
+cd src
+```
+编译：
+```
+sudo make clean && sudo make
+```
+编译成功的话：
+显示：
+
+```
+jmh@ubuntu:~/RBOS/src$ sudo make clean && make
+make -C /lib/modules/4.4.0/build M=/home/jmh/RBOS/src clean
+make[1]: Entering directory `/usr/src/linux-4.4'
+  CLEAN   /home/jmh/RBOS/src/.tmp_versions
+  CLEAN   /home/jmh/RBOS/src/Module.symvers
+make[1]: Leaving directory `/usr/src/linux-4.4'
+make -C /lib/modules/4.4.0/build M=/home/jmh/RBOS/src modules
+make[1]: Entering directory `/usr/src/linux-4.4'
+  CC [M]  /home/jmh/RBOS/src/hellomd.o
+/home/jmh/RBOS/src/hellomd.c:261:2: warning: initialization from incompatible pointer type [enabled by default]
+  LSM_HOOK_INIT(inode_mkdir,sample_inode_mkdir),
+  ^
+/home/jmh/RBOS/src/hellomd.c:261:2: warning: (near initialization for ‘demo_hooks[4].hook.inode_mkdir’) [enabled by default]
+  Building modules, stage 2.
+  MODPOST 1 modules
+  CC      /home/jmh/RBOS/src/hellomd.mod.o
+  LD [M]  /home/jmh/RBOS/src/hellomd.ko
+make[1]: Leaving directory `/usr/src/linux-4.4'
+```
+说明编译成功。
+5. 加载驱动即可使RBOS生效
+进入RBOS/src/ 目录
+```
+insmod hellomd.ko
+```
+没有任何输出即说明没有出错。
+
+# 实现原理
+1. 实现LSM模块,在系统的5个系统调用上进行hook。例如对文件删除系统调用进行hook.
+首先需要定位出 rm 命令在删除文件时会使用到的系统调用,可以通过strace命令最终rm命令的执行过程。
+例如：
+```
+jmh@ubuntu:~$ strace rm test 
+execve("/bin/rm", ["rm", "test"], [/* 59 vars */]) = 0
+brk(0)                                  = 0x1267000
+access("/etc/ld.so.nohwcap", F_OK)      = -1 ENOENT (No such file or directory)
+mmap(NULL, 8192, PROT_READ|PROT_WRITE, MAP_PRIVATE|MAP_ANONYMOUS, -1, 0) = 0x7f655af12000
+access("/etc/ld.so.preload", R_OK)      = -1 ENOENT (No such file or directory)
+open("/etc/ld.so.cache", O_RDONLY|O_CLOEXEC) = 3
+fstat(3, {st_mode=S_IFREG|0644, st_size=89943, ...}) = 0
+mmap(NULL, 89943, PROT_READ, MAP_PRIVATE, 3, 0) = 0x7f655aefc000
+close(3)                                = 0
+access("/etc/ld.so.nohwcap", F_OK)      = -1 ENOENT (No such file or directory)
+open("/lib/x86_64-linux-gnu/libc.so.6", O_RDONLY|O_CLOEXEC) = 3
+read(3, "\177ELF\2\1\1\0\0\0\0\0\0\0\0\0\3\0>\0\1\0\0\0P \2\0\0\0\0\0"..., 832) = 832
+fstat(3, {st_mode=S_IFREG|0755, st_size=1840928, ...}) = 0
+mmap(NULL, 3949248, PROT_READ|PROT_EXEC, MAP_PRIVATE|MAP_DENYWRITE, 3, 0) = 0x7f655a92d000
+mprotect(0x7f655aae7000, 2097152, PROT_NONE) = 0
+mmap(0x7f655ace7000, 24576, PROT_READ|PROT_WRITE, MAP_PRIVATE|MAP_FIXED|MAP_DENYWRITE, 3, 0x1ba000) = 0x7f655ace7000
+mmap(0x7f655aced000, 17088, PROT_READ|PROT_WRITE, MAP_PRIVATE|MAP_FIXED|MAP_ANONYMOUS, -1, 0) = 0x7f655aced000
+close(3)                                = 0
+mmap(NULL, 4096, PROT_READ|PROT_WRITE, MAP_PRIVATE|MAP_ANONYMOUS, -1, 0) = 0x7f655aefb000
+mmap(NULL, 8192, PROT_READ|PROT_WRITE, MAP_PRIVATE|MAP_ANONYMOUS, -1, 0) = 0x7f655aef9000
+arch_prctl(ARCH_SET_FS, 0x7f655aef9740) = 0
+mprotect(0x7f655ace7000, 16384, PROT_READ) = 0
+mprotect(0x60d000, 4096, PROT_READ)     = 0
+mprotect(0x7f655af14000, 4096, PROT_READ) = 0
+munmap(0x7f655aefc000, 89943)           = 0
+brk(0)                                  = 0x1267000
+brk(0x1288000)                          = 0x1288000
+open("/usr/lib/locale/locale-archive", O_RDONLY|O_CLOEXEC) = 3
+fstat(3, {st_mode=S_IFREG|0644, st_size=7216688, ...}) = 0
+mmap(NULL, 7216688, PROT_READ, MAP_PRIVATE, 3, 0) = 0x7f655a24b000
+close(3)                                = 0
+ioctl(0, SNDCTL_TMR_TIMEBASE or SNDRV_TIMER_IOCTL_NEXT_DEVICE or TCGETS, {B38400 opost isig icanon echo ...}) = 0
+newfstatat(AT_FDCWD, "test", {st_mode=S_IFREG|0664, st_size=6, ...}, AT_SYMLINK_NOFOLLOW) = 0
+geteuid()                               = 1000
+newfstatat(AT_FDCWD, "test", {st_mode=S_IFREG|0664, st_size=6, ...}, AT_SYMLINK_NOFOLLOW) = 0
+faccessat(AT_FDCWD, "test", W_OK)       = 0
+unlinkat(AT_FDCWD, "test", 0)           = 0
+lseek(0, 0, SEEK_CUR)                   = -1 ESPIPE (Illegal seek)
+close(0)                                = 0
+close(1)                                = 0
+close(2)                                = 0
+exit_group(0)                           = ?
++++ exited with 0 +++
+
+```
+可以很清楚的看出 unlinkat 是删除文件时关键系统调用.通过查看unlinkat的说明,也可以佐证这一点。
+```
+       The unlinkat() system call operates in exactly the same way  as  either
+       unlink(2)  or  rmdir(2) (depending on whether or not flags includes the
+       AT_REMOVEDIR flag) except for the differences described in this  manual
+       page.
+```
+同时,unlink也是一个删除文件时常用的系统调用;
+
+因此,我们可以确定要hook的系统调用为：unlink以及unlinkat,通过在这两个函数加入自定义的删除权限判断即可。
+
+如果发现当前进程的current->uid和euid是有相应权限的,那么调用原unlink函数,返回对应unlink执行的结果；否则,返回错误信息即可。
+
+3. 将角色,权限,用户ID之间的关系作为配置文件写入文件系统内,在内核中读配置文件，解析其中的内容，得到不同ID的用户对应的权限。
+4. 在check_perm函数中 获取当前进程的实际用户ID,然后根据第三步得到的结果,得到实际用户的权限。再在具体的check*_perm函数内检测对应的right有没有所需要的特权位。
+
---
+++ RBOS/src/hellomd.c
@@ -0,0 +1,575 @@
+/*
+ * A LSM modules which implements role based access control.
+ */
+#include <linux/module.h>
+#include <linux/init.h>
+#include <linux/kernel.h>
+#include <linux/ptrace.h>
+#include <linux/errno.h>
+#include <linux/sched.h>
+#include <linux/security.h>
+#include <linux/xattr.h>
+#include <linux/capability.h>
+#include <linux/unistd.h>
+#include <linux/mm.h>
+#include <linux/mman.h>
+#include <linux/slab.h>
+#include <linux/pagemap.h>
+#include <linux/swap.h>
+#include <linux/uaccess.h>
+#include <linux/spinlock.h>
+#include <linux/syscalls.h>
+#include <linux/file.h>
+#include <linux/namei.h>
+#include <linux/mount.h>
+#include <linux/ext2_fs.h>
+#include <linux/proc_fs.h>
+#include <linux/kd.h>
+#include <linux/netfilter_ipv4.h>
+#include <linux/netfilter_ipv6.h>
+#include <linux/tty.h>
+#include <net/icmp.h>
+#include <net/ip.h>		/* for sysctl_local_port_range[] */
+#include <net/tcp.h>		/* struct or_callable used in sock_rcv_skb */
+#include <asm/uaccess.h>
+#include <asm/ioctls.h>
+#include <linux/bitops.h>
+#include <linux/interrupt.h>
+#include <linux/netdevice.h>	/* for network interface checks */
+#include <linux/netlink.h>
+#include <linux/tcp.h>
+#include <linux/udp.h>
+#include <linux/quota.h>
+#include <linux/un.h>		/* for Unix socket types */
+#include <net/af_unix.h>	/* for Unix socket types */
+#include <linux/parser.h>
+#include <linux/nfs_mount.h>
+#include <net/ipv6.h>
+#include <linux/hugetlb.h>
+#include <linux/personality.h>
+#include <linux/sysctl.h>
+#include <linux/audit.h>
+#include <linux/string.h>
+#include <linux/unistd.h>
+#include <linux/lsm_hooks.h>
+#include <linux/syscalls.h>
+#include <linux/kallsyms.h>
+#include <linux/sched.h>
+#include <asm/uaccess.h>
+#include <asm/unistd.h>
+#include <linux/kernel.h>
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/syscalls.h>
+#include <linux/file.h>
+#include <linux/fs.h>
+#include <linux/fcntl.h>
+#include <linux/version.h>
+#include <linux/syscalls.h>
+
+//定义将要hook的系统调用
+#define SYSCALL_CONNECT		0x01
+#define SYSCALL_SOCKET		0x02
+#define SYSCALL_MKDIR		0x04
+#define SYSCALL_RMDIR		0x08
+#define SYSCALL_TASK_CREATE 0x10
+
+//定义四种角色
+#define ROLE_RECYCLER_NAME "recycler"
+#define ROLE_OPERATOR_NAME "operator"
+#define ROLE_NETMANAGER_NAME "netmanager"
+#define ROLE_ADMIN_NAME "admin"
+
+#define ROLE_RECYCLER 	1
+#define ROLE_OPERATOR 	2
+#define ROLE_NETMANAGER 3
+#define ROLE_ADMIN 		4
+
+//定义几个配置文件的路径,此处是硬编码的
+#define ROLE_CONFIG		"/etc/rbos/role_config"
+#define USER_CONFIG		"/etc/rbos/user_config"
+
+//最多支持16种角色,128个用户的分配
+#define SAMPLE_MAX_ROLE 	16
+#define SAMPLE_MAX_USER		128
+
+#define SAMPLE_MAX_BUF		1024
+
+
+//角色结构
+typedef struct __role__
+{
+	unsigned int roleid;
+	unsigned int right;
+}_ROLE_STRUCT;
+_ROLE_STRUCT all_roles[SAMPLE_MAX_ROLE]={{0,0},};
+unsigned int all_roles_cnt=0;
+
+//用户结构
+typedef struct __user__
+{
+	unsigned int userid;
+	unsigned int right;
+}_USER_STRUCT;
+
+_USER_STRUCT all_users[SAMPLE_MAX_USER]={{0,SYSCALL_CONNECT | SYSCALL_SOCKET | SYSCALL_MKDIR |SYSCALL_RMDIR | SYSCALL_TASK_CREATE},};
+unsigned int all_users_cnt=1;
+
+typedef union {
+	struct _socket_info{
+		int domain;
+		int type;
+		int protocol;
+	};
+	struct _connect_info {
+		struct socket *sock;
+		struct sockaddr *address;
+		int addrlen;
+	}connect_info;
+	
+	struct _mkdir_info {
+		struct inode *dir;
+		struct dentry *dentry;
+		int mask;
+	}mkdir_info;
+	
+	struct _rmdir_info {
+		struct inode *dir;
+		struct dentry *dentry;
+	}rmdir_info;
+	struct _taskcreate_info{
+		unsigned long clone_flags;
+	};
+}perm_info_t;
+
+
+unsigned int sample_asc2int(char * str,int len)
+//将整形字符串转换为整数
+{
+	unsigned int rst =0;
+	int i;
+	for(i =0;i<len;i++)
+	{
+		rst=rst * 10 + str[i]-'0';
+	}
+	printk(KERN_WARNING "asc2int::%d,len:%d  str:%s.\n",rst,len,str);
+	return rst;
+}
+int check_connect_perm(perm_info_t *info,unsigned int right)
+{
+	printk(KERN_WARNING "___Check connect permission___:: %s\n", __FUNCTION__);
+	if(right & SYSCALL_CONNECT) 
+	{
+		return 0;
+	}
+	else
+	{
+		return -EINVAL;
+	}
+}
+
+int check_socket_perm(perm_info_t *info,unsigned int right)
+{
+	printk(KERN_WARNING "___Check socket create permission___:: %s\n", __FUNCTION__);
+	if(right&SYSCALL_SOCKET)
+	{
+		return 0;
+	}
+	else
+	{
+		return -EINVAL;
+	}
+}
+
+int check_taskcreate_perm(perm_info_t *info,unsigned int right)
+{
+	printk(KERN_WARNING "___Check task_create permission___:: %s\n", __FUNCTION__);
+	if(right & SYSCALL_TASK_CREATE)
+	{
+		return 0;
+	}
+	else
+	{
+		return -EINVAL;
+	}
+	
+}
+
+
+int check_mkdir_perm(perm_info_t *info,unsigned int right)
+{
+	printk(KERN_WARNING "___Check mkdir permission___:: %s\n", __FUNCTION__);
+	if(right & SYSCALL_MKDIR)
+	{
+		return 0;
+	}
+	else
+	{
+		return -EINVAL;
+	}
+}
+
+int check_rmdir_perm(perm_info_t *info,unsigned int right)
+{
+	printk(KERN_WARNING "___Check rmdir permission___:: %s\n", __FUNCTION__);
+	if(right & SYSCALL_RMDIR)
+	{
+		return 0;
+	}
+	else
+	{
+		return -EINVAL;
+	}
+}
+
+
+static int check_perm(int syscall_type, perm_info_t *perm_info)
+{
+	int ret=0;
+	struct cred * new;
+	unsigned int uid=0;
+	unsigned int right=0;
+	
+	
+	//获取用户实际uid,并且得到用户的权限,默认用户都拥有SYSCALL_TASK_CREATE的权限
+	//注意并没有使用有效用户id;因为有效用户id会通过setuid位升权限
+	new=prepare_creds();
+	uid =(unsigned int)(new->uid).val;
+	int i;
+	for( i=0;i<all_users_cnt;i++)
+	{
+		if(all_users[i].userid == uid)
+		{
+			right |= all_users[i].right;
+			break;
+		}
+	}
+	if(right==0)
+		//非测试用户,打开所有权限
+	{
+		right=SYSCALL_CONNECT | SYSCALL_SOCKET | SYSCALL_MKDIR |SYSCALL_RMDIR | SYSCALL_TASK_CREATE;
+	}
+	printk(KERN_WARNING "____Check Permission___::%s, real uid :: %d.\n", __FUNCTION__,uid);
+	switch (syscall_type) {
+	case SYSCALL_CONNECT:
+		ret = check_connect_perm(perm_info,right);
+		break;
+		
+	case SYSCALL_SOCKET:
+		ret = check_socket_perm(perm_info,right);
+		break;
+
+	case SYSCALL_TASK_CREATE:
+		ret = check_taskcreate_perm(perm_info,right);
+		break;
+
+	case SYSCALL_MKDIR:
+		ret = check_mkdir_perm(perm_info,right);
+		break;
+
+	case SYSCALL_RMDIR:
+		ret = check_rmdir_perm(perm_info,right);
+		break;
+	}
+	
+	return ret;  
+}
+
+static int sample_socket_connect(struct socket *sock, struct sockaddr *address, int addrlen)
+{
+	perm_info_t perm_info;
+
+	
+	return check_perm(SYSCALL_CONNECT, &perm_info);
+}
+
+static int sample_socket(int domain,int type,int protocol,int kern)
+//TODO 完善参数列表
+{
+	perm_info_t perm_info;
+
+	return check_perm(SYSCALL_SOCKET,&perm_info);
+}
+
+static int sample_task_create(unsigned long clone_flags)
+{
+	perm_info_t perm_info;
+
+	return check_perm(SYSCALL_TASK_CREATE,&perm_info);
+}
+
+static int sample_inode_mkdir(struct inode *dir, struct dentry *dentry, int mask)
+{
+	perm_info_t perm_info; 
+
+
+	
+	return check_perm(SYSCALL_MKDIR, &perm_info);
+	
+}
+
+static int sample_inode_rmdir(struct inode *dir, struct dentry *dentry)
+{
+	perm_info_t perm_info; 
+
+
+	
+	return check_perm(SYSCALL_RMDIR, &perm_info);
+}
+
+
+static void get_role_config(void)
+{
+	char buf[SAMPLE_MAX_BUF]={0};
+	struct file * f=NULL;
+	const char * filename =ROLE_CONFIG;
+	char * p;
+	int i=0;
+	char * line_start;
+	char * token_start;
+	printk(KERN_INFO "get role config from %s.\n",filename);
+	mm_segment_t oldfs;
+
+	f =filp_open(filename,O_RDONLY,0);
+	if( IS_ERR(f) || (f==NULL))
+	{
+		printk(KERN_WARNING "get role config error.\n");
+		return ;
+	}
+	p=buf;
+	line_start = buf;
+	token_start=buf;
+	int role_index =0;
+	oldfs =get_fs();
+	//printk(KERN_INFO "%s::%d.\n",__FUNCTION__,__LINE__);
+	set_fs(get_ds());
+	//printk(KERN_INFO "%s::%d.\n",__FUNCTION__,__LINE__);
+	while(vfs_read(f,buf+i,1,&f->f_pos)==1)
+	{
+		//printk(KERN_INFO "%s::%d.THE SRC:%s\n",__FUNCTION__,__LINE__,buf);
+		if(i==SAMPLE_MAX_BUF)
+			//读满缓存区
+		{
+			break;
+		}
+		if(buf[i]==':')
+			//读到角色
+		{
+			//printk(KERN_INFO "%s::%d.\n",__FUNCTION__,__LINE__);
+			buf[i]=0;//把":"截断
+			printk(KERN_INFO "read a role of :%s.\n",line_start);
+			
+			all_roles_cnt++;
+			if(role_index != 0)
+			{
+				printk(KERN_INFO "last role right:%x.\n",all_roles[role_index].right);
+			}
+			if(strcmp((const char *)line_start,ROLE_ADMIN_NAME)==0)
+			{
+				role_index = ROLE_ADMIN;
+			}
+			//printk(KERN_INFO "%s::%d.\n",__FUNCTION__,__LINE__);
+			if(strcmp((const char *)line_start,ROLE_NETMANAGER_NAME)==0)
+			{
+				role_index = ROLE_NETMANAGER;
+			}
+			//printk(KERN_INFO "%s::%d.\n",__FUNCTION__,__LINE__);
+			if(strcmp((const char *)line_start,ROLE_OPERATOR_NAME)==0)
+			{
+				role_index =ROLE_OPERATOR;
+			}
+			//printk(KERN_INFO "%s::%d.\n",__FUNCTION__,__LINE__);
+			if(strcmp((const char *)line_start,ROLE_RECYCLER_NAME)==0)
+			{
+				role_index=ROLE_RECYCLER;
+			}
+			token_start = buf+i+1;
+		}
+		if(buf[i]=='\n')
+			//遇到换行符了
+		{
+			line_start = buf +i +1;
+		}
+		//printk(KERN_INFO "%s::%d.\n",__FUNCTION__,__LINE__);
+		if(buf[i]==',' || buf[i]==';')
+			//到了一个token的终点
+		{
+			//[token_start,buf+i)是一个token
+			buf[i]=0;
+			if((buf+i-token_start)<4)
+				//明显不是一个token
+			{
+				;
+			}
+			else
+			{
+				
+				//printk(KERN_INFO "%s::%d.\n",__FUNCTION__,__LINE__);
+				if(strcmp((const char *)token_start,"SYSCALL_CONNECT")==0)
+					//具有SYSCALL_CONNECT权限
+				{
+					all_roles[role_index].right |=SYSCALL_CONNECT;
+				}
+				//printk(KERN_INFO "%s::%d.\n",__FUNCTION__,__LINE__);
+				if(strcmp((const char *)token_start,"SYSCALL_SOCKET")==0)
+				{
+					all_roles[role_index].right |= SYSCALL_SOCKET;
+				}
+				//printk(KERN_INFO "%s::%d.\n",__FUNCTION__,__LINE__);
+				if(strcmp((const char *)token_start,"SYSCALL_MKDIR")==0)
+				{
+					all_roles[role_index].right |=SYSCALL_MKDIR;
+				}
+				//printk(KERN_INFO "%s::%d.\n",__FUNCTION__,__LINE__);
+				if(strcmp((const char *)token_start,"SYSCALL_RMDIR")==0)
+				{
+					all_roles[role_index].right |=SYSCALL_RMDIR;
+				}
+				//printk(KERN_INFO "%s::%d.\n",__FUNCTION__,__LINE__);
+				if(strcmp((const char *)token_start,"SYSCALL_TASK_CREATE")==0)
+				{
+					all_roles[role_index].right |= SYSCALL_TASK_CREATE;
+				}
+			}
+			token_start=buf+i+1;
+		}
+		i++;
+	}
+	set_fs(oldfs);
+	filp_close(f,0);
+
+	printk(KERN_INFO "load %d roles.\n",all_roles_cnt);
+}
+
+
+static void get_user_config(void)
+{
+	char buf[SAMPLE_MAX_BUF]={0};
+	struct file * f=NULL;
+	const char * filename =USER_CONFIG;
+	char * p;
+	int i=0;
+	char * line_start;
+	char * token_start;
+	printk(KERN_INFO "get user config from %s.\n",filename);
+	mm_segment_t oldfs;
+
+	f =filp_open(filename,O_RDONLY,0);
+	if( IS_ERR(f) || (f==NULL))
+	{
+		printk(KERN_WARNING "get user config error.\n");
+		return ;
+	}
+	p=buf;
+	line_start = buf;
+	token_start=buf;
+	int user_index =0;
+	oldfs =get_fs();
+	//printk(KERN_INFO "%s::%d.\n",__FUNCTION__,__LINE__);
+	set_fs(KERNEL_DS);
+	//printk(KERN_INFO "%s::%d.\n",__FUNCTION__,__LINE__);
+	while(vfs_read(f,buf+i,1,&f->f_pos)==1)
+	{
+		//printk(KERN_INFO "%s::%d.\n",__FUNCTION__,__LINE__);
+		if(i==SAMPLE_MAX_BUF)
+			//读满缓存区
+		{
+			break;
+		}
+		if(buf[i]==':')
+			//读到用户了
+		{
+			unsigned int userid = sample_asc2int(line_start,buf+i-line_start);
+			printk(KERN_INFO "read userid of %d.\n",userid);
+			if(all_users_cnt)
+			{
+				printk(KERN_INFO "last user ::%d,right:%x.\n",all_users[all_users_cnt-1].userid,all_users[all_users_cnt-1].right);
+			}
+			all_users[all_users_cnt++].userid =userid;
+			user_index = all_users_cnt-1;
+			token_start = buf+i+1;
+		}
+		//TODO
+		if(buf[i]=='\n')
+			//遇到换行符了
+		{
+			line_start = buf +i +1;
+		}
+		if(buf[i]==',' || buf[i]==';')
+			//到了一个token的终点
+		{
+			//[token_start,buf+i)是一个token
+			buf[i]=0;
+			if((buf+i-token_start)<5)
+				//明显不是一个token,这有可能为空
+			{
+				;
+			}
+			else
+			{
+				if(strcmp((const char *)token_start,ROLE_ADMIN_NAME)==0)
+				{
+					all_users[user_index].right |=all_roles[ROLE_ADMIN].right;
+				}
+				if(strcmp((const char *)token_start,ROLE_OPERATOR_NAME)==0)
+				{
+					all_users[user_index].right |=all_roles[ROLE_OPERATOR].right;
+				}
+				if(strcmp((const char *)token_start,ROLE_RECYCLER_NAME)==0)
+				{
+					all_users[user_index].right |=all_roles[ROLE_RECYCLER].right;
+				}
+				if(strcmp((const char *)token_start,ROLE_NETMANAGER_NAME)==0)
+				{
+					all_users[user_index].right |=all_roles[ROLE_NETMANAGER].right;
+				}
+			}
+			token_start=buf+i+1;
+		}
+		i++;
+	}
+		set_fs(oldfs);
+	filp_close(f,0);
+
+	printk(KERN_INFO "load %d user.\n",all_users_cnt);
+}
+
+static struct security_hook_list demo_hooks[]=
+{
+	LSM_HOOK_INIT(socket_connect,sample_socket_connect),
+	LSM_HOOK_INIT(socket_create,sample_socket),
+	LSM_HOOK_INIT(task_create,sample_task_create),
+	LSM_HOOK_INIT(inode_mkdir,sample_inode_mkdir),
+	LSM_HOOK_INIT(inode_rmdir,sample_inode_rmdir)
+};
+
+
+static  int sample_init(void)
+{
+
+	printk(KERN_INFO "ADD LSM SAMPLE.\n");
+	printk(KERN_INFO "LOAD ROLE CONFIG.\n");//载入role config;
+	get_role_config();
+	printk(KERN_INFO "LOAD USER CONFIG.\n");//载入user config;
+	get_user_config();
+	security_add_hooks(demo_hooks,ARRAY_SIZE(demo_hooks));
+	printk(KERN_INFO "Sample:  Initializing.\n");
+
+	return 0;
+}
+
+static  void sample_exit(void)
+{
+	printk(KERN_INFO "Sample: Exiting.\n");	
+}
+
+
+
+module_init(sample_init);
+
+
+module_exit(sample_exit);
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("JMH");
+MODULE_DESCRIPTION("A LSM Driver implements RBAC");
